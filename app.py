3f4e7715cc22b1cafdc7d8bc82be8bd8a2fe92e3
Miguel Cam <miguel.cam.mx@gmail.com>
2021-01-03 12:30:03 -0600

    Config database :fire:

--- app.py
+++ app.py
@@ -6,7 +6,11 @@
 # ==============================================================
 
 # -*- coding: utf-8 -*-
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect, url_for, session, request, logging
+from flask_mysqldb import MySQL
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from passlib.hash import sha256_crypt
+
 
 app = Flask(__name__)
 
@@ -17,6 +21,16 @@ def index():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+class RegisterForm(Form):
+    name = StringField('Name', [validators.Length(min=1, max=50)])
+    username = StringField('Username', [validators.Length(min=4, max=25)])
+    email = StringField('Email', [validators.Length(min=6, max=50)])
+    password = PasswordField('Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Passwords do not match')
+    ])
+    confirm = PasswordField('Confirm Password')
 
 if __name__ == '__main__':
     app.run(debug=True)
