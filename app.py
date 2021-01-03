5de12c5702e3dcede9bde9c52f85eb544bd2eb97
Miguel Cam <miguel.cam.mx@gmail.com>
2021-01-02 18:15:12 -0600

    Initial commit. :beers:

---
+++ app.py
@@ -0,0 +1,22 @@
+# ==============================================================
+# Author: Miguel Camacho
+# Email: miguel.cam.mx@gmail.com
+# Twitter: @elmiguelmx
+#
+# ==============================================================
+
+# -*- coding: utf-8 -*-
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+@app.route('/about.html')
+def about():
+    return render_template('about.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
